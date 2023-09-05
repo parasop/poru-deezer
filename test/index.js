ae1cf2e14f625aede8cc85beb96562930987d548
parasop <63485824+parasop@users.noreply.github.com>
2023-09-05 09:16:14 +0000

    done

--- test/index.js
+++ test/index.js
@@ -1,5 +1,12 @@
 const {Deezer} = require("../dist/index.js");
 
 const deezer = new Deezer();
-const data = deezer.resolve({query:"https://deezer.page.link/swdx3ipfyvMueDrK9"})
+const data = deezer.resolve({query:"tum hi ho",source:"deezer",requester:"test"})
 //.then(x => console.log(x))
+
+
+const Data = require("./demo.js")
+
+const test = new Data()
+
+test.getTrackMediaURI()
